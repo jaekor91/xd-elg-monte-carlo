8fdef4fb3ba4660144edafff5d2209fdbb788935
Jae Hyeon Lee <jaekor91@gmail.com>
2017-08-31 16:48:04 -0400

    Added the application of window function.

--- combine-Tractor-bricks.py
+++ combine-Tractor-bricks.py
@@ -11,9 +11,10 @@ import sys
 large_random_constant = -999119283571
 deg2arcsec=3600
 
-dr_v = "3" # Data release version
+dr_v = "5" # Data release version
 data_directory = "/Users/jaehyeon/Documents/Research/ELG_target_selection/data-repository/DR"+dr_v+"/"
 tycho_directory = "/Users/jaehyeon/Documents/Research/ELG_target_selection/data-repository/"
+windowf_directory= "/Users/jaehyeon/Documents/Research/ELG_target_selection/data-repository/DEEP2/windowf/"
 
 ##############################################################################	
 print("Combine all Tractor files by field, append Tycho-2 stellar mask column and mask objects using DEEP2 window funtions.")
@@ -27,7 +28,22 @@ tracf4 = combine_tractor_nocut(data_directory+"f4/")
 print("Completed.")
 
 
-print("2. Append Tycho2 stark mask field.")
+print("2. Impose DEEP2 window functions.")
+# Field 2
+idx = np.logical_or(window_mask(tracf2["ra"], tracf2["dec"], windowf_directory+"windowf.21.fits"), window_mask(tracf2["ra"], tracf2["dec"], windowf_directory+"windowf.22.fits"))
+tracf2_trimmed = tracf2[idx]
+
+# Field 3
+idx = np.logical_or.reduce((window_mask(tracf3["ra"], tracf3["dec"], windowf_directory+"windowf.31.fits"), window_mask(tracf3["ra"], tracf3["dec"], windowf_directory+"windowf.32.fits"),window_mask(tracf3["ra"], tracf3["dec"], windowf_directory+"windowf.33.fits")))
+tracf3_trimmed = tracf3[idx]
+
+# Field 4
+idx = np.logical_or(window_mask(tracf4["ra"], tracf4["dec"], windowf_directory+"windowf.41.fits"), window_mask(tracf4["ra"], tracf4["dec"], windowf_directory+"windowf.42.fits"))
+tracf4_trimmed = np.copy(tracf4[idx])
+print("Completed.")
+
+
+print("3. Append Tycho2 stark mask field.")
 # Field 2 
 tracf2 = apply_tycho(tracf2, tycho_directory+"tycho2.fits", galtype="ELG")
 # Field 3
@@ -40,7 +56,7 @@ print("Completed.")
 
 
 ##############################################################################	
-print("3. Save the trimmed catalogs.")
+print("4. Save the trimmed catalogs.")
 # Field 2
 cols = fits.ColDefs(tracf2)
 tbhdu = fits.BinTableHDU.from_columns(cols)
