9efcb847863a706c13e55f4389bd24db0c3e5830
Jae Hyeon Lee <jaekor91@gmail.com>
2017-08-31 19:21:04 -0400

    Fixed a few things about how the data are processed and what the columns are called. In particular, Field 4 bad objects are removed.

--- produce-deep2-f234-photo-redz-oii.py
+++ produce-deep2-f234-photo-redz-oii.py
@@ -11,7 +11,11 @@ large_random_constant = -999119283571
 deg2arcsec=3600
 
 # Data directory (set by the user)
-data_directory = "./"
+data_directory = "/Users/jaehyeon/Documents/Research/ELG_target_selection/data-repository/DEEP2/pcats/"
+zcat_directory = "/Users/jaehyeon/Documents/Research/ELG_target_selection/data-repository/DEEP2/redz-oii-catalog/"
+color_directory = "/Users/jaehyeon/Documents/Research/ELG_target_selection/data-repository/DEEP2/"
+windowf_directory= "/Users/jaehyeon/Documents/Research/ELG_target_selection/data-repository/DEEP2/windowf/"
+
 
 cnames = ["Gold", "Silver", "LowOII", "NoOII", "LowZ", "NoZ", "D2reject", "","D2unobserved"]
 
@@ -66,7 +70,7 @@ pcat2_good = pcat2[pcat2["BADFLAG"]==0]
 pcat3_good = pcat3[pcat3["BADFLAG"]==0]
 
 # Field 4
-pcat4_good = pcat4
+pcat4_good = pcat4[pcat4["BADFLAG"]==0]
 
 print("Completed.\n")
 
@@ -74,15 +78,15 @@ print("Completed.\n")
 ##############################################################################
 print("3. Mask objects that lie outside DEEP2 window functions.")
 # Field 2
-idx = np.logical_or(window_mask(pcat2_good["RA_DEEP"], pcat2_good["DEC_DEEP"], "windowf.21.fits"), window_mask(pcat2_good["RA_DEEP"], pcat2_good["DEC_DEEP"], "windowf.22.fits"))
+idx = np.logical_or(window_mask(pcat2_good["RA_DEEP"], pcat2_good["DEC_DEEP"], windowf_directory+"windowf.21.fits"), window_mask(pcat2_good["RA_DEEP"], pcat2_good["DEC_DEEP"], windowf_directory+"windowf.22.fits"))
 pcat2_trimmed = pcat2_good[idx]
 
 # Field 3
-idx = np.logical_or.reduce((window_mask(pcat3_good["RA_DEEP"], pcat3_good["DEC_DEEP"], "windowf.31.fits"), window_mask(pcat3_good["RA_DEEP"], pcat3_good["DEC_DEEP"], "windowf.32.fits"),window_mask(pcat3_good["RA_DEEP"], pcat3_good["DEC_DEEP"], "windowf.33.fits")))
+idx = np.logical_or.reduce((window_mask(pcat3_good["RA_DEEP"], pcat3_good["DEC_DEEP"], windowf_directory+"windowf.31.fits"), window_mask(pcat3_good["RA_DEEP"], pcat3_good["DEC_DEEP"], windowf_directory+"windowf.32.fits"),window_mask(pcat3_good["RA_DEEP"], pcat3_good["DEC_DEEP"], windowf_directory+"windowf.33.fits")))
 pcat3_trimmed = pcat3_good[idx]
 
 # Field 4
-idx = np.logical_or(window_mask(pcat4_good["RA_DEEP"], pcat4_good["DEC_DEEP"], "windowf.41.fits"), window_mask(pcat4_good["RA_DEEP"], pcat4_good["DEC_DEEP"], "windowf.42.fits"))
+idx = np.logical_or(window_mask(pcat4_good["RA_DEEP"], pcat4_good["DEC_DEEP"], windowf_directory+"windowf.41.fits"), window_mask(pcat4_good["RA_DEEP"], pcat4_good["DEC_DEEP"], windowf_directory+"windowf.42.fits"))
 pcat4_trimmed = np.copy(pcat4_good[idx])
 
 print("Completed.\n")
@@ -112,15 +116,15 @@ print("Completed.\n")
 print("5. Estimate the spectroscopic area using window functions.")
 print("File names: windowf.**.fits")
 # Field 2
-area_f2 = est_spec_area("windowf.21.fits")+est_spec_area("windowf.22.fits")
+area_f2 = est_spec_area(windowf_directory+"windowf.21.fits")+est_spec_area(windowf_directory+"windowf.22.fits")
 print("Field 2 area: %.3f" % area_f2)
 
 # Field 3
-area_f3 = est_spec_area("windowf.31.fits")+est_spec_area("windowf.32.fits")+est_spec_area("windowf.33.fits")
+area_f3 = est_spec_area(windowf_directory+"windowf.31.fits")+est_spec_area(windowf_directory+"windowf.32.fits")+est_spec_area(windowf_directory+"windowf.33.fits")
 print("Field 3 area: %.3f" % area_f3)
 
 # Field 4
-area_f4 = est_spec_area("windowf.41.fits")+est_spec_area("windowf.42.fits")
+area_f4 = est_spec_area(windowf_directory+"windowf.41.fits")+est_spec_area(windowf_directory+"windowf.42.fits")
 print("Field 4 area: %.3f" % area_f4)
 
 # Total
@@ -135,7 +139,7 @@ print("color-selection.txt: Catalog that provides DEEP2 BRI color selection info
     Contains object ID, RA, dec, whether the object would have been targeted if in EGS. \n \
     (1=yes, 0=no), and whether it would have been targeted in a non-EGS field.\n \
     Provided by Jeff Newman")
-DEEP2color = ascii.read("color-selection.txt")
+DEEP2color = ascii.read(color_directory+"color-selection.txt")
 DEEP2color_OBJNO = np.asarray(DEEP2color["col1"])
 DEEP2color_ra = np.asarray(DEEP2color["col2"])
 DEEP2color_dec = np.asarray(DEEP2color["col3"])
@@ -151,13 +155,13 @@ print("deep2-f**-redz-oii.fits: DEEP2 redshift catalogs that John Moustakas prov
     \t-2.0 = line not measured (not in spectral range)\n \
 	Note 2: For ZQUALITY values, see http://deep.ps.uci.edu/DR4/zquality.html.")
 # Field 2
-f2_objno, f2_ra, f2_dec, f2_oii, f2_oii_err, f2_z, f2_z_err, f2_zquality, f2_weight = import_zcat("deep2-f2-redz-oii.fits")
+f2_objno, f2_ra, f2_dec, f2_oii, f2_oii_err, f2_z, f2_z_err, f2_zquality, f2_weight = import_zcat(zcat_directory+"deep2-f2-redz-oii.fits")
 
 # Field 3
-f3_objno, f3_ra, f3_dec, f3_oii, f3_oii_err, f3_z, f3_z_err, f3_zquality, f3_weight = import_zcat("deep2-f3-redz-oii.fits")
+f3_objno, f3_ra, f3_dec, f3_oii, f3_oii_err, f3_z, f3_z_err, f3_zquality, f3_weight = import_zcat(zcat_directory+"deep2-f3-redz-oii.fits")
 
 # Field 4
-f4_objno, f4_ra, f4_dec, f4_oii, f4_oii_err, f4_z, f4_z_err, f4_zquality, f4_weight = import_zcat("deep2-f4-redz-oii.fits")
+f4_objno, f4_ra, f4_dec, f4_oii, f4_oii_err, f4_z, f4_z_err, f4_zquality, f4_weight = import_zcat(zcat_directory+"deep2-f4-redz-oii.fits")
 print("Completed.\n")
 
 
@@ -165,7 +169,7 @@ print("Completed.\n")
 ##############################################################################
 print("7. Append additional columns to the photometric catalogs from others.")
 print("7a. Append redshift catalogs.")
-col_name_list = ["OBJNO_zcat", "RA_zcat", "DEC_zcat", "OII_3727","OII_3727_ERR", "RED_Z", "Z_ERR", "ZQUALITY_JOHN", "TARG_WEIGHT"]
+col_name_list = ["OBJNO_zcat", "RA_zcat", "DEC_zcat", "OII_3727","OII_3727_ERR", "RED_Z", "Z_ERR", "Z_QUALITY", "TARG_WEIGHT"]
 print("Columns added: " + ", ".join(col_name_list))
 # Field 2
 pcat2 = fits.open("deep2-f2-photo-trimmed.fits")[1].data
@@ -183,7 +187,6 @@ for i in range(len(new_col_list)):
 
 # Field 4
 del pcat4
-col_name_list = ["OBJNO_zcat", "RA_zcat", "DEC_zcat", "OII_3727","OII_3727_ERR", "RED_Z", "Z_ERR", "ZQUALITY_JOHN", "TARG_WEIGHT"]
 pcat4 = fits.open("deep2-f4-photo-trimmed.fits")[1].data
 idx1, idx2 = match_objno(pcat4["OBJNO"], f4_objno)
 new_col_list = [f4_objno, f4_ra, f4_dec, f4_oii, f4_oii_err, f4_z, f4_z_err, f4_zquality, f4_weight]
@@ -194,9 +197,9 @@ print("f2: # in zcat minus # in pcat matched %d" % (f2_objno.size-(pcat2["RED_Z"
 print("f3: # in zcat minus # in pcat matched %d" % (f3_objno.size-(pcat3["RED_Z"]>-1000).sum()))
 print("f4: # in zcat minus # in pcat matched %d" % (f4_objno.size-(pcat4["RED_Z"]>-1000).sum()))    
 print("The number of overlapping objects are smaller because certain\n \
-	spectroscopic areas were masked out in previous steps (the area\n \
-	estimates above are compatible). I ignore the small number of object\n \
-	loss. The object loss ")
+spectroscopic areas were masked out in previous steps (the area\n \
+estimates above are compatible). I ignore the small number of object\n \
+loss.")
 print("Completed.\n")
 
 
@@ -223,7 +226,7 @@ for i in range(len(new_col_list)):
 print("f2: # in pcat minus # in pcat matched %d" % (pcat2.shape[0]-(pcat2["BRI_cut"]>-1000).sum()))
 print("f3: # in pcat minus # in pcat matched %d" % (pcat3.shape[0]-(pcat3["BRI_cut"]>-1000).sum()))
 print("f4: # in pcat minus # in pcat matched %d" % (pcat4.shape[0]-(pcat4["BRI_cut"]>-1000).sum()))    
-print("The last 251 objects will be classified as DEEP2 BRI color rejected objects.")
+print("The last 223 objects will be classified as DEEP2 BRI color rejected objects.")
 print("Completed.\n")
 
 
