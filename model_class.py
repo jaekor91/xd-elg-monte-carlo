9041f6464c4c3de2a1e5e6036431ef36355210ff
Jae Hyeon Lee <jaekor91@gmail.com>
2017-10-15 21:42:44 -0400

    Added forgotten AND.

--- model_class.py
+++ model_class.py
@@ -2833,8 +2833,8 @@ class model3(parent_model):
         # ELG (DESI and NonDESI)
         i=2
         samples = np.array([self.var_x_obs[i], self.var_y_obs[i], self.gmag_obs[i]]).T
-        w_DESI = (self.redz_obs[i]>0.6) & (self.redz_obs[i]<1.6) (self.oii_obs[i]>8) # Only objects in the correct redshift and OII ranges.
-        w_NonDESI = (self.redz_obs[i]>0.6) & (self.redz_obs[i]<1.6) (self.oii_obs[i]<8) # Only objects in the correct redshift and OII ranges.
+        w_DESI = (self.redz_obs[i]>0.6) & (self.redz_obs[i]<1.6) & (self.oii_obs[i]>8) # Only objects in the correct redshift and OII ranges.
+        w_NonDESI = (self.redz_obs[i]>0.6) & (self.redz_obs[i]<1.6) & (self.oii_obs[i]<8) # Only objects in the correct redshift and OII ranges.
         MD_hist_N_ELG_DESI, _ = np.histogramdd(samples, bins=self.num_bins, range=[self.var_x_limits, self.var_y_limits, self.gmag_limits], weights=w_DESI)
         MD_hist_N_ELG_NonDESI, _ = np.histogramdd(samples, bins=self.num_bins, range=[self.var_x_limits, self.var_y_limits, self.gmag_limits], weights=w_NonDESI)
         FoM_tmp, _ = np.histogramdd(samples, bins=self.num_bins, range=[self.var_x_limits, self.var_y_limits, self.gmag_limits], weights=self.FoM_obs[i])
