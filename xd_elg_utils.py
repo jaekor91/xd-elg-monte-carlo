799c99c3a59ada8aab9bc13de45a58a831c69108
Jae Hyeon Lee <jaekor91@gmail.com>
2017-11-13 10:25:22 -0500

    Fixed the bug with the importance sampling weights.

--- xd_elg_utils.py
+++ xd_elg_utils.py
@@ -2202,7 +2202,7 @@ def gen_err_seed(nsample, sigma=1, return_iw_factor=False):
     """
     if return_iw_factor:
         err_seed = np.random.normal(0, sigma, nsample)
-        r_tilde = np.exp(-err_seed**2 * (1/2. - 1/float(sigma))) # p(x)/q(x), both un-normalized
+        r_tilde = np.exp(-err_seed**2 * (1/2. - 1/float(2*sigma**2))) # p(x)/q(x), both un-normalized
         iw_factor = r_tilde # Do not use "/r_tilde.sum()" to normaliz
         return err_seed, iw_factor
     else:
